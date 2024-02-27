917ac271210150e959948c7e8c81110c9beda67b
Senthil <senthilnathant@hotmail.com>
2024-02-27 18:38:50 +0530

    Update main-ui.js

--- synthpack/main-ui.js
+++ synthpack/main-ui.js
@@ -9,10 +9,10 @@
 	/***************************************************************************************************************************************************/
 
     // File links on web server	
-	var url_sounds_erase_sounds_command_padded_bin = "https://senthilnathant1984.github.io/playtime/synthpack/erase_sounds_jan_2024";
-	var url_sounds_sounds_only_padkey_bin = "https://senthilnathant1984.github.io/playtime/synthpack/synthpack_padkey";
+	var url_sounds_erase_sounds_command_padded_bin = "https://www.playtimeengineering.com/synthpack/erase_sounds_jan_2024";
+	var url_sounds_sounds_only_padkey_bin = "https://www.playtimeengineering.com/synthpack/synthpack_padkey";
 
-    var DELAY_AFTER_SENDING_FIRST_FILE_IN_MILLISECONDS = 35000;	//Was 2000						// 50 milliseconds
+  var DELAY_AFTER_SENDING_FIRST_FILE_IN_MILLISECONDS = 35000;	//Was 2000						// 50 milliseconds
 	var DELAY_AFTER_SENDING_EVERY_CHUNKOF_SECOND_FILE_IN_MILLISECONDS = 2;	//5 works		// 50 milliseconds.  Can't see much betweeo 1 and 2.  Saw a fail with 1.  try 2.  2 saw fail.. back to 5.
 	
     var CHUNK_SIZE_OF_SECOND_FILE = 256;		// was 256
