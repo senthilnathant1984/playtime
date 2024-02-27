aa119509788099c43dff97265223344bc4a525d5
Senthil <senthilnathant@hotmail.com>
2024-02-27 14:23:49 +0530

    Upload latest files

---
+++ mytracks/main-ui.js
@@ -0,0 +1,317 @@
+(function() {
+
+    var imported = document.createElement("script");
+	imported.src = "usbuploader-lib.js";
+	document.getElementsByTagName("head")[0].appendChild(imported);
+
+    /***************************************************************************************************************************************************/
+	// Programmable variables - Start
+	/***************************************************************************************************************************************************/
+
+    // File links on web server	
+	var url_sounds_erase_sounds_command_padded_bin = "https://www.playtimeengineering.com/synthpack/erase_sounds_jan_2024";
+	var url_sounds_sounds_only_padkey_bin = "https://www.playtimeengineering.com/synthpack/synthpack_padkey";
+
+    var DELAY_AFTER_SENDING_FIRST_FILE_IN_MILLISECONDS = 35000;	//Was 2000						// 50 milliseconds
+	var DELAY_AFTER_SENDING_EVERY_CHUNKOF_SECOND_FILE_IN_MILLISECONDS = 2;	//5 works		// 50 milliseconds.  Can't see much betweeo 1 and 2.  Saw a fail with 1.  try 2.  2 saw fail.. back to 5.
+	
+    var CHUNK_SIZE_OF_SECOND_FILE = 256;		// was 256
+
+    /***************************************************************************************************************************************************/
+	// Programmable variables - End
+	/***************************************************************************************************************************************************/
+
+    // Local variables
+    var ui = {
+        buttonSearchDevice: null,
+		textSearchStatus:null,
+		buttonConnectDisconnect: null,
+		textConnectionStatus:null,
+		buttonUploadFile: null,
+		progressbarUpload:null,  
+		progressBarFillUpload:null,
+		textUploadStatus:null,
+	};	
+
+    let finalPorts = []; 	// has properties 'portName' a string value and 'port' an port object
+	var numberOfPorts = 0;
+	var bSendingFileInProgress = false;
+
+        // Update UI
+	var initializeWindow = function() {
+		console.log("initializeWindow is called");
+	  
+		for (var k in ui) {
+		  var id = k;
+		  //console.log(id);
+		  var element = document.getElementById(id);
+		  if (!element) {
+			  throw "Missing UI element: " + k;
+			}
+			ui[k] = element;
+			//console.log(ui[k]);
+		}
+		
+		for (var k in ui) {
+			console.log(ui[k]);
+		}
+		
+        setProgressBar(20);
+
+		ui.buttonSearchDevice.addEventListener('click', onButtonSearchDeviceClicked);
+		ui.buttonConnectDisconnect.addEventListener('click', onButtonConnectDisconnectClicked);
+		ui.buttonUploadFile.addEventListener('click', onButtonUploadFileClicked);
+		
+        enableSearchDeviceButton(false);
+        ui.textSearchStatus.innerHTML = "";
+
+        enableConnectDisconnectButton(false);
+        ui.textConnectionStatus.innerHTML = "";
+
+        enableUploadFileButton(false);
+        ui.textUploadStatus.innerHTML = "";
+
+        displayUploadProgressbar(false);
+		
+		if ("serial" in navigator) {
+			// The Web Serial API is supported.
+			console.log("Serial Web API is supported");
+			enableSearchDeviceButton(true);
+		} else {
+			console.log("Serial Web API is not supported");
+			alert("Browser not supported, must use Chrome.");			
+			enableSearchDeviceButton(false);
+            ui.textSearchStatus.innerHTML = "";
+			return;
+		}
+		
+		// The following call will find and add the ports to the ports list if 
+		// they are already paired / permitted by the user
+		enumeratePorts_ui();
+	};
+
+    window.addEventListener('load', initializeWindow);
+
+    var onButtonSearchDeviceClicked = async function() {
+		try {
+			// Add a new port with the permission obtained from user
+			await RequestUSBComPort();
+			enumeratePorts_ui();		
+		}
+		catch (err) {
+		    enumeratePorts_ui();	
+		}
+	};
+
+    var onButtonConnectDisconnectClicked = async function() {
+        try {
+			var buttonText = ui.buttonConnectDisconnect.innerHTML;
+			if (buttonText == "Connect to myTRACKS") {
+				// Connect to the device
+				// Open the port
+				var selectedPortInfo = finalPorts[0];
+				await OpenPort(selectedPortInfo);
+				if (selectedPortInfo.port.writable) {
+					// Port opened and it is writable
+					// Set the text of Device Connect Disconnect button box
+					ui.buttonConnectDisconnect.innerHTML = "DISCONNECT myTRACKS"
+					// Set the text of Device Connect Disconnect Status box
+					ui.textConnectionStatus.innerHTML = "myTRACKS connected";
+					// Enable download buttons
+					enableUploadFileButton(true);
+				} else {
+					// Failure in port open for writing
+					// Set the text of Device Connect Disconnect Status box
+					ui.textConnectionStatus.innerHTML = "";
+					alert("Can't open device, another application is using this device.");
+					enableUploadFileButton(false);
+				}				
+			} else {
+				// Disconnect the device
+				// Close the port
+				var selectedPortInfo = finalPorts[0];
+				await ClosePort(selectedPortInfo);
+				ui.buttonConnectDisconnect.innerHTML = "Connect to myTRACKS"
+				ui.textConnectionStatus.innerHTML = "";
+				enableUploadFileButton(false);
+                displayUploadProgressbar(false);
+                ui.textUploadStatus.innerHTML = "";
+			}
+		}
+		catch (err) {	
+			alert(err);
+			//alert("Can't open device, another application is using this device.");
+		}
+    };
+	
+    var onButtonUploadFileClicked = async function() {
+        try {
+			if (bSendingFileInProgress == true) {
+				alert("File transfer in progress, please wait till it completes.");
+				return;
+			}
+			await WriteFilesToPort("sounds.bin", url_sounds_erase_sounds_command_padded_bin, url_sounds_sounds_only_padkey_bin);
+		}
+		catch (err) {
+			console.log(err);
+		}
+    };
+
+    var WriteFilesToPort = async function(fileType, firstFileUrl, secondFileUrl) {
+		try {
+			// Send the first file - specified by the url firstFileUrl to the device, then wait for a delay of 
+			// DELAY_AFTER_SENDING_FIRST_FILE_IN_MILLISECONDS then send the second file specified by the url secondFileUrl
+			
+			// Receive the first file - specified by the url firstFileUrl from the server and send to the device
+			bSendingFileInProgress = true;
+            displayUploadProgressbar(true);
+			var oRequest = new XMLHttpRequest();
+			oRequest.open("GET", firstFileUrl, true);
+			oRequest.responseType = "arraybuffer";
+			oRequest.onload = async function (oEvent) {
+				var arrayBuffer = oRequest.response;
+				if (arrayBuffer) {
+					var byteArray = new Uint8Array(arrayBuffer);
+					var selectedPortInfo = finalPorts[0];
+					await WriteToPort(selectedPortInfo, byteArray, false);		// Don't close the write stream of serial port here, close at the last WriteToPort call where we finish writing all the data
+					console.log("First file sent to port");		
+					
+					// First file sent to the device. next send the second file after a delay of 
+					// DELAY_AFTER_SENDING_FIRST_FILE_IN_MILLISECONDS
+					
+					// Wait for a delay of DELAY_AFTER_SENDING_FIRST_FILE_IN_MILLISECONDS
+					await Sleep(DELAY_AFTER_SENDING_FIRST_FILE_IN_MILLISECONDS);
+					console.log("done with first file delay");	
+					var oRequest1 = new XMLHttpRequest();
+					oRequest1.open("GET", secondFileUrl, true);
+					oRequest1.responseType = "arraybuffer";
+					oRequest1.onload = async function (oEvent) { 
+						var arrayBuffer = oRequest1.response;
+						if (arrayBuffer) {
+							const sizeOfBuffer = arrayBuffer.byteLength;							
+							var numberOfChunks = (sizeOfBuffer - sizeOfBuffer % CHUNK_SIZE_OF_SECOND_FILE) / CHUNK_SIZE_OF_SECOND_FILE;
+							var numberOfBytesRemaining = sizeOfBuffer % CHUNK_SIZE_OF_SECOND_FILE;
+							if (numberOfBytesRemaining > 0) { 
+								numberOfChunks = numberOfChunks + 1;
+							}
+							var start = 0;
+							var end = 0;
+							for (var i = 0; i < numberOfChunks - 1; i++) {
+								start = i * CHUNK_SIZE_OF_SECOND_FILE;
+								end = start + CHUNK_SIZE_OF_SECOND_FILE;
+								const byteArray = new Uint8Array(arrayBuffer.slice(start, end));
+								var selectedPortInfo = finalPorts[0];
+								await WriteToPort(selectedPortInfo, byteArray, false);
+							//	if (i % 2 === 0){
+								//console.log("delaying");
+								await Sleep(DELAY_AFTER_SENDING_EVERY_CHUNKOF_SECOND_FILE_IN_MILLISECONDS);
+							//	}
+							}
+							start = end;
+							end = sizeOfBuffer;
+							const byteArray = new Uint8Array(arrayBuffer.slice(start, end));
+							var selectedPortInfo = finalPorts[0];
+							await WriteToPort(selectedPortInfo, byteArray, true);
+							console.log("Second file sent to port");
+							Sleep(500);
+                            
+							ui.textUploadStatus.innerHTML = "File Upload Complete, restart myTRACKS.";
+							
+							bSendingFileInProgress = false;
+						}
+					};
+					oRequest1.send();
+				}
+			};
+			oRequest.send();
+		}
+		catch (err) {
+			if (fileType == "software.bin") {
+				ui.buttonSendFileSoftwareStatus.innerHTML = "File Not Sent";
+			} else if (fileType == "sounds.bin") {
+				ui.buttonSendFileSoundsStatus.innerHTML = "File Not Sent";
+			} else if (fileType == "sets.bin") {
+				ui.buttonSendFileSetsStatus.innerHTML = "File Not Sent";
+			}
+			console.log(err);
+		}
+	}
+
+
+    // Get all supported Playtime End Studio devices
+	// Though we collect all supported devices, we use only the first device in the List
+	// since multiple device support is not required
+	var enumeratePorts_ui =  async function () {
+	    console.log("enumeratePorts_ui start");	    
+	    finalPorts = await EnumeratePorts();
+	    numberOfPorts = finalPorts.length;
+		onPortsEnumerated(finalPorts);			
+		console.log("enumeratePorts_ui exit");
+	};
+
+	var onPortsEnumerated = function (ports) {
+		
+	    console.log("onPortsEnumerated start");   
+
+		if (numberOfPorts > 0) {
+			// Device found, enable Connect button
+			// Enable Connect button
+			enableConnectDisconnectButton(true);
+			// Set the text of Search Device Status
+			ui.textSearchStatus.innerHTML = "myTRACKS found";
+		} else {
+			enableConnectDisconnectButton(false);			
+            enableUploadFileButton(false);
+			// Set the text of Search Device Status box
+			ui.textSearchStatus.innerHTML = "";
+		}
+	    
+	    console.log("onPortsEnumerated exit");
+	};
+
+    var enableSearchDeviceButton = function (bEnable) {
+       if (bEnable == true) {
+            ui.buttonSearchDevice.disabled = false;
+            ui.buttonSearchDevice.style.backgroundColor  = "#425CC7";
+       } else {
+            ui.buttonSearchDevice.disabled = true;
+            ui.buttonSearchDevice.style.backgroundColor  = "#53565A";
+       }
+    };
+
+    var enableConnectDisconnectButton = function (bEnable) {
+        if (bEnable == true) {
+            ui.buttonConnectDisconnect.disabled = false;
+            ui.buttonConnectDisconnect.style.backgroundColor  = "#425CC7";
+        } else {
+            ui.buttonConnectDisconnect.disabled = true;
+            ui.buttonConnectDisconnect.style.backgroundColor  = "#53565A";
+        }
+    };
+
+     var enableUploadFileButton = function (bEnable) {
+        if (bEnable == true) {
+            ui.buttonUploadFile.disabled = false;
+            ui.buttonUploadFile.style.backgroundColor  = "#425CC7";
+        } else {
+            ui.buttonUploadFile.disabled = true;
+            ui.buttonUploadFile.style.backgroundColor  = "#53565A";
+        }
+     };
+
+     var displayUploadProgressbar = function(bDisplay) {
+        if (bDisplay == true) {
+            ui.progressbarUpload.style.display = 'block';
+        } else {
+            ui.progressbarUpload.style.display = 'none';
+        }
+     }
+     
+     function setProgressBar(progressPercent) {
+        // Ensure the input is within 0 to 100 percent
+        const clampedProgress = Math.min(100, Math.max(0, progressPercent));      
+        // Set the width of the progress bar fill element to the desired percentage
+        ui.progressBarFillUpload.style.width = clampedProgress + '%';
+    }
+
+}());
---
+++ mytracks/usbuploader-lib.js
@@ -0,0 +1,125 @@
+﻿/***************************************************************************************************************************************************/
+// Programmable variables - Start
+/***************************************************************************************************************************************************/
+var PLAYTIME_ENG_STUDIO_DEVICE_USB_VID = 0x03EB;
+var PLAYTIME_ENG_STUDIO_DEVICE_USB_PID = 0xECAD;
+var PORT_PARAM_BAUDRATE = 9999999;
+var PORT_PARAM_BUFFERSIZE = 4;
+var PORT_PARAM_DATABITS = 8;
+var PORT_PARAM_STOPBITS = 1;
+var PORT_PARAM_PARITY = "none";
+var PORT_PARAM_FLOWCONTROL = "hardware";
+
+
+//1024:  1 blinkg
+//512
+//64= 7
+//128 = about 26
+//4096= quick fail
+//8 working much better
+//1, quick fail
+//2, fail after about 5 mins
+//4, fail after 1 min
+//2, almost works
+//2 failed on PC with 15 ms delay
+
+
+
+/***************************************************************************************************************************************************/
+// Programmable variables - End
+/***************************************************************************************************************************************************/
+
+// Local variable
+var numberOfPorts = 0;
+var DEVICE_NAME_PLAYTIME_ENG_STUDIO = "Playtime Eng Studio";
+
+/***************************************************************************************************************************************************/
+// Playtime USBUploader Calls - Start
+/***************************************************************************************************************************************************/
+
+// RequestUSBComPort
+// Description:     This function gets the permission from the user to connect the Playtime Eng Studio Device (Serial Port) to the Web application. 
+//                  This function call is a must before using any other Playtime USBUploader Library functions.
+// Arguments:       None
+// Return Value:    None
+async function RequestUSBComPort() {	
+	// First request permission for Playtime Eng Studio Device (This is nothing but a USB Virtual COM Port)	
+	const vendorId = PLAYTIME_ENG_STUDIO_DEVICE_USB_VID; 	// Playtime Eng Studio Device USB VID
+    const productId = PLAYTIME_ENG_STUDIO_DEVICE_USB_PID;  	// Playtime Eng Studio Device USB PID	
+	const filters = [
+		{ usbVendorId: vendorId, usbProductId: productId }
+	];
+	try {
+		await navigator.serial.requestPort({ filters });
+	} catch (e) {
+		console.log("Exception: " + e);
+	}    
+}
+
+// EnumeratePorts
+// Description:     This function gets port object information for the Playtime Eng Studio devices connected.
+// Arguments:       None
+// Return Value:    Array of Serial port information object. Each item in the array represents the port information of each port. 
+//                  The port object and port name would be returned in the port information object.
+async function EnumeratePorts() {	
+	// Get USB Serial port device list
+    let ports = await navigator.serial.getPorts();
+    numberOfPorts = 0;
+    let detectedPorts = [];    
+    for (var port of ports) {
+        do {
+            var portName = null;
+			const { usbProductId, usbVendorId } = port.getInfo();
+            if (usbVendorId == PLAYTIME_ENG_STUDIO_DEVICE_USB_VID && usbProductId == PLAYTIME_ENG_STUDIO_DEVICE_USB_PID) {
+				portName = DEVICE_NAME_PLAYTIME_ENG_STUDIO;
+			}
+
+            if (portName !== null) {
+                var detectedPort = new Object();
+                detectedPort.portName = portName + " Device " + (numberOfPorts + 1);
+                detectedPort.port = port;
+                detectedPorts[numberOfPorts] = detectedPort;            
+                numberOfPorts++;
+            }
+        } while (false);
+    }	
+    return detectedPorts;
+}
+
+async function OpenPort(portInfo) {
+	var port = portInfo.port;
+	await port.open({ 
+		baudRate: PORT_PARAM_BAUDRATE,
+		dataBits: PORT_PARAM_DATABITS,
+		stopBits: PORT_PARAM_STOPBITS,
+		parity: PORT_PARAM_PARITY,
+		bufferSize: PORT_PARAM_BUFFERSIZE,
+		flowControl: PORT_PARAM_FLOWCONTROL
+	});
+}
+
+async function ClosePort(portInfo) {
+	var port = portInfo.port;	
+	await port.close();
+}
+
+async function WriteToPort(portInfo, abyDataBuffer, closeWriteStream) {
+	var port = portInfo.port;
+	const writer = port.writable.getWriter();	
+	await writer.write(abyDataBuffer).then( function() {
+			// if successful
+			// Allow the serial port to be closed later
+			if (closeWriteStream == true) {
+				writer.close();				
+			}
+			writer.releaseLock();
+		}).catch( function(error) {
+			// if some error
+			console.log("Error occurred while write - " + error);
+		});
+}
+/***************************************************************************************************************************************************/
+// Playtime USBUploader Calls - End
+/***************************************************************************************************************************************************/
+
+    
