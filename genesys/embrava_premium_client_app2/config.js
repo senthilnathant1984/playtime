42a86c66b329000eeb71ae7a39265b2f51ceb6d5
SENTHILNATHAN THANGAVEL <senthilnathant@hotmail.com>
2026-06-04 14:17:21 +0530

    Update config.js

--- genesys/embrava_premium_client_app2/config.js
+++ genesys/embrava_premium_client_app2/config.js
@@ -1,6 +1,6 @@
 export default {
     clientID: '168cb942-8b8a-4a5d-84e0-4c65c30d3e8f',
-    basePath: 'https://embrava.github.io/genesys/embrava_premium_client_app2/',
+    basePath: 'https://senthilnathant1984.github.io/playtime/genesys/embrava_premium_client_app2/',
 	  appName: 'premium-app-embrava',
 	
 	  defaultPcEnvironment: 'mypurecloud.com',
